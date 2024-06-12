88ecd451cc559f02e1327636c998751fa376203d
Artemi_grou@mail.ru <Artemi_grou@mail.ru>
2024-06-12 11:22:52 +1000

    buba

---
+++ DemoExamen/ViewModels/OrdersListVM.cs
@@ -0,0 +1,21 @@
+﻿using DemoExamen.Database;
+using DemoExamen.Models;
+using DemoExamen.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoExamen.ViewModels
+{
+    public class OrdersListVM : BaseVM
+    {
+        public List<Order> Orders { get; set; }
+
+        public OrdersListVM()
+        {
+            Orders = DemoExamenBDContext.GetInstance().Orders.ToList();
+        }  
+    }
+}
